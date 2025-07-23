2fc3e6b96552d3eaf5c81fd2933154fa3c1a828f
mickeyLalata <mckyl1108@gmail.com>
2025-07-23 22:19:02 +0800

    changed to production mode and install ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "./src/entry.js",
     output: {
         filename: "main.js",
